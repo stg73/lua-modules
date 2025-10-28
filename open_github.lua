68b05f87bddc6d23943a98b91c6171e8c9f461cb
STG <stg737467@gmail.com>
2025-10-28 12:34:08 +0900

    change(open_github): 複数の文字列を取って開く関数を削除
    "get_url.url"とこの関数"get_url.str"は機能がほとんど重複しており 両方維持する必要がない
    URLっぽい文字列が使える"get_url.url"の方が使いやすいので "get_url.str"を削除した

--- open_github.lua
+++ open_github.lua
@@ -17,29 +17,19 @@ function M.get_url.tbl(tbl)
     return "github.com/" .. tbl.repo .. "/raw/" .. hoge(tbl.commit) .. "/" .. tbl.file
 end
 
-function M.get_url.str(repo) return function(commit) return function(file)
-    return "github.com/" .. repo .. "/raw/" .. hoge(commit) .. "/" .. file
-end end end
-
 function M.get_url.url(str)
     local repo = r.gsub("/")("/.")(r.match("[^//]+")(str))
     local commit = r.gsub("/")("/.")(r.match("//@<=[^//]+")(str))
     local file = r.remove(".{-}//.{-}//")(str)
-    return M.get_url.str(repo)(commit)(file)
+    return "github.com/" .. repo .. "/raw/" .. hoge(commit) .. "/" .. file
 end
 
 function M.open(str_or_tbl)
     if type(str_or_tbl) == "table" then
         w.open(M.get_url.tbl(str_or_tbl))
     else
-        return function(commit) return function(file)
-            w.open(M.get_url.str(str_or_tbl)(commit)(file))
-        end end
+        w.open(M.get_url.url(str_or_tbl))
     end
-end
-
-function M.open_url(str)
-    w.open(M.get_url.url(str))
 end
 
 return M
