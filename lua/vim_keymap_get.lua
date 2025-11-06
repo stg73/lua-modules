7b1094f89de9bceb275947c530629fe425288cae
STG <stg737467@gmail.com>
2025-11-06 09:38:57 +0900

    improve(vim_keymap_get): オプションが使えるように
    キーマップを再帰的に取得するオプションを追加

--- lua/vim_keymap_get.lua
+++ lua/vim_keymap_get.lua
@@ -9,10 +9,17 @@ function M.get_string(mode,lhs)
     return r.remove("^\n.{3}/V" .. lhs .. "/v.{3}")(map_line) -- マッピングの部分を取得
 end
 
-function M.get(mode,lhs)
+function M.get(mode,lhs,opts)
+    -- 引数の既定値
+    local opts = opts or {}
+
     local mapping = t.match(function(t) return t.lhs == lhs end)(vim.api.nvim_get_keymap(mode))
     if mapping then
-        return mapping.rhs or mapping.callback
+        local rhs = mapping.rhs or mapping.callback
+        if opts.remap and (mapping.noremap == 0 or r.has("^/V/c<plug>")(rhs)) then -- remap オプションが指定されており実際にリマップされている場合
+            return M.get(mode,rhs,opts)
+        end
+        return rhs
     else
         return nil
     end
