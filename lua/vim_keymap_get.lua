96bd24e419cd716a84ff65634b7bf2be99eab011
STG <stg737467@gmail.com>
2025-11-07 09:06:50 +0900

    feat(vim_keymap_get): オプションを追加

--- lua/vim_keymap_get.lua
+++ lua/vim_keymap_get.lua
@@ -14,15 +14,22 @@ function M.get(mode,lhs,opts)
     local opts = opts or {}
 
     local mapping = t.match(function(t) return t.lhs == lhs end)(vim.api.nvim_get_keymap(mode))
-    if mapping then
-        local rhs = mapping.rhs or mapping.callback
-        if opts.remap and (mapping.noremap == 0 or r.has("^/V/c<plug>")(rhs)) then -- remap オプションが指定されており実際にリマップされている場合
-            return M.get(mode,rhs,opts)
-        end
-        return rhs
-    else
+
+    if mapping == nil then
         return nil
     end
+
+    local rhs = mapping.rhs or mapping.callback
+
+    if opts.remap and (mapping.noremap == 0 or r.has("^/V/c<plug>")(rhs)) then -- remap オプションが指定されており実際にリマップされている場合
+        return M.get(mode,rhs,opts)
+    end
+
+    if opts.expr and mapping.expr == 0 then
+        return nil
+    end
+
+    return rhs
 end
 
 return M
