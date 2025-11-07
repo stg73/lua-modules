7ca14f4838fd583d318f5a87df10c353a53ddd2b
STG <stg737467@gmail.com>
2025-11-07 09:42:37 +0900

    change(vim_keymap_get): get_string()を削除
    理由: get()で十分

--- lua/vim_keymap_get.lua
+++ lua/vim_keymap_get.lua
@@ -3,12 +3,6 @@ local M = {}
 local r = require("regex")
 local t = require("tbl")
 
-function M.get_string(mode,lhs)
-    local map_result = vim.fn.execute(mode .. "map " .. lhs) -- map コマンドの結果
-    local map_line = r.match("\n.  /V" .. lhs .. " /v[^\n]+")(map_result) -- 部分一致するマッピングも表示されるので完全一致するものだけ取得
-    return r.remove("^\n.{3}/V" .. lhs .. "/v.{3}")(map_line) -- マッピングの部分を取得
-end
-
 function M.get(mode,lhs,opts)
     -- 引数の既定値
     local opts = opts or {}
